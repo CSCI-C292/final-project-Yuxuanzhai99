f802b28cd54bbd075c76316fb396a4abdd8b998c
Yuxuanzhai99 <71047180+Yuxuanzhai99@users.noreply.github.com>
2020-12-13 18:36:49 -0500

    fin

---
+++ final game/Assets/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]float _speed=2f;
+        private void Update()
+    {
+        transform.position -= new Vector3(0, Time.deltaTime * _speed, 0);
+
+    }
+}
